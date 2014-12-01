06fd3a8af8ef6744126481de3d1c8f5dfa149763
silt99 <sltapia@gmail.com>
2014-12-01 01:22:56 +0100

    test_1

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-print ("Hello!")
-print ("Hello!World!")
-print ("Hello!World!")
+import unittest
+from google.appengine.api import memcache
+from google.appengine.ext import db
+from google.appengine.ext import testbed
+
+class TestModel(db.Model):
+  """A model class used for testing."""
+  number = db.IntegerProperty(default=42)
+  text = db.StringProperty()
+
+class TestEntityGroupRoot(db.Model):
+  """Entity group root"""
+  pass
+
+def GetEntityViaMemcache(entity_key):
+  """Get entity from memcache if available, from datastore if not."""
+  entity = memcache.get(entity_key)
+  if entity is not None:
+    return entity
+  entity = TestModel.get(entity_key)
+  if entity is not None:
+    memcache.set(entity_key, entity)
+  return entity
